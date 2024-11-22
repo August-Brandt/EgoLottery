3c41613c6a222debd4a149aa488b90217fca97e2
August-Brandt <augustbrandt170@gmail.com>
2024-11-22 13:11:37 +0100

    Read file with folders and list content

--- src/main.go
+++ src/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var foldersFile string
+	flag.StringVar(&foldersFile, "file", "", "path to file containing folder to look for .git folders in")
+	flag.Parse()
+	if foldersFile == "" { // Default file if non given
+		foldersFile = "~/.config/egolottery/folders"
+	}
+
+	file, err := os.Open(foldersFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+	folders := strings.Split(string(data), "\n")
+	for _, value := range folders {
+		PrintPath(value)
+	}
+}
+
+func PrintPath(path string) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s:\n", path)
+	for _, entry := range entries {
+		fmt.Printf("\t%s\n", entry.Name())
+	}
 }
