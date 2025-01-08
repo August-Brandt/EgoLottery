f44185b763523580f4929852f0cbb5d167d14df1
August-Brandt <augustbrandt170@gmail.com>
2025-01-08 18:33:00 +0100

    Add testutil to create commit in repo

--- src/testutils/testutils.go
+++ src/testutils/testutils.go
@@ -1,10 +1,16 @@
 package testutils
 
 import (
+	"fmt"
+	"math/rand/v2"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 func CreateTestDirectory() (string, error) {
@@ -44,4 +50,39 @@ func CreateTestDirectoryFromPath(path string) (string, error) {
 		return name, err
 	}
 	return name, nil
+}
+
+func AddCommitInRepo(path string) error {
+	num := []byte(fmt.Sprintf("%d", rand.IntN(9999999)))
+	err := os.WriteFile(filepath.Join(path, "README.md"), num, 0644)
+	if err != nil {
+		return err
+	}
+	
+	repo, err := git.PlainOpenWithOptions(filepath.Join(path, ".git"), &git.PlainOpenOptions{DetectDotGit: true})
+	if err != nil {
+		return err
+	}
+	worktree, err := repo.Worktree()
+	if err != nil {
+		return err
+	}
+
+	_, err = worktree.Add(".")
+	if err != nil {
+		return err
+	}
+	
+	_, err = worktree.Commit(fmt.Sprint(num), &git.CommitOptions{
+		Author: &object.Signature{
+			Name: "John Doe",
+			Email: "john@doe.org",
+			When: time.Now(),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
