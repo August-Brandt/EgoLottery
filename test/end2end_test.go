7fffe29ec849cbb78cd09de2a44573cae212c10e
August Brandt <augustbrandt170@gmail.com>
2025-04-09 15:29:59 +0200

    Add e2e test using dirs flag

--- test/end2end_test.go
+++ test/end2end_test.go
@@ -16,7 +16,7 @@ func buildProgram(t *testing.T) {
 	}
 }
 
-func TestEndToEnd(t *testing.T) {
+func TestEndToEndFile(t *testing.T) {
 	buildProgram(t)
 	defer os.Remove("egolottery")
 
@@ -31,6 +31,34 @@ func TestEndToEnd(t *testing.T) {
 	defer os.Remove("./testDirs")
 
 	cmd := exec.Command("./egolottery", "-file", "./testDirs", "-depth", "1")
+	out, err := cmd.Output()
+	if err != nil {
+		t.Fatalf("Error while running program: %v\n", err)
+	}
+	outString := string(out)
+
+	splitOut := strings.Split(outString, "\n")
+	if splitOut[0] != ".git directories found:" {
+		t.Errorf("Incorrect output compared to expected\n\tActual:\n%s\n\tExpected:\n.git directories found:\n", outString)
+	}
+	if splitOut[1] != filepath.Join(currentDir, ".git") {
+		t.Errorf("Incorrect output compared to expected\n\tActual:\n%s\n\tExpected:\n%s\n", outString, filepath.Join(currentDir, ".git"))
+	}
+	if len(splitOut) < 2 {
+		t.Errorf("Output too short")
+	}
+}
+
+func TestEndToEndDirs(t *testing.T) {
+	buildProgram(t)
+	defer os.Remove("egolottery")
+
+	currentDir, err := filepath.Abs("..")
+	if err != nil {
+		t.Fatalf("Error while getting current directory: %v\n", err)
+	}
+
+	cmd := exec.Command("./egolottery", "-dirs", currentDir, "-depth", "1")
 	out, err := cmd.Output()
 	if err != nil {
 		t.Fatalf("Error while running program: %v\n", err)
